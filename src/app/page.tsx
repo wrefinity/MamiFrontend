77ad9a77631e34dd8041738b3984eefb2d9b7ff1
doniecliff <doniecliff143@gmail.com>
2023-06-29 14:08:26 +0100

    first commit- mami-mini-css-framework
    starts with m- (prefix)  p, m, gap

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,7 +5,7 @@ export default function Home() {
   return (
     <main className={styles.main}>
       <h1>MamiHub - <span>Quality Nigerian Made Product You can Trust</span></h1>
-      <button>Shop Now</button>
+      <button className='border-it px-8'>Shop Now</button>
     </main>
   )
 }
