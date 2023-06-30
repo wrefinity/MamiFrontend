38a7db23c957f61e8893664d9ffc157c5fd1fe0c
doniecliff <doniecliff143@gmail.com>
2023-06-30 23:49:03 +0100

    new changes header

--- src/components/FilterBar.tsx
+++ src/components/FilterBar.tsx
@@ -5,27 +5,54 @@ import Link from 'next/link'
 import Image from 'next/image'
 import mamiLogo from '../../assets/img/Mami_Logo-removebg-preview 1.png'
 import searchIcon from '../../assets/icons/search.svg'
+import help from '../../assets/icons/help-support.svg'
+import angleDown from '../../assets/icons/down-angle.svg'
+import account from '../../assets/icons/Account Icon.svg'
+import cart from '../../assets/icons/cart.svg'
+
 
 
 function FilterBar() {
   return (
   <section className={style['search-bar'] + ' outline-it'} rel="search bar">
     <div className='outline-it'>
-      <div>
-        <Image src={mamiLogo} alt="MamiHub Brand logo" />
+      <div className={style.logo}>
+        <Image className='w-100' src={mamiLogo} alt="MamiHub Brand logo" />
       </div>
+        
+      <div className={style.sell}>
         <Link href='#'>sell on MamiHub</Link>
-        
-      <div>
-        <input type="text" />
-        <button>
-          <Image src={searchIcon} alt="" />
-        </button>
+        <div className={style['search-container']}>
+          <input type="text" />
+          <button>
+            <Image src={searchIcon} alt="" />
+          </button>
+        </div>    
       </div>
         
-      <div></div>
-      <div></div>
-      <div></div>
+      <nav className={style['filter-nav']}>
+        <button>
+          <div>
+            <Image className='w-100' src={help} alt="" />
+          </div>
+            Help
+          <div>
+            <Image className='w-100' src={angleDown} alt="" />
+          </div>
+        </button> 
+        <Link href=''>
+          <div>
+            <Image className='w-100' src={account} alt="" />
+          </div>
+          Account
+        </Link>
+        <Link href=''>
+          <div>
+            <Image className='w-100' src={cart} alt="" />
+          </div>
+          Cart
+        </Link>
+      </nav>
     </div>
   </section>
   )
--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,9 +1,34 @@
+import Link from 'next/link';
 import React from 'react'
+import style from './NavBar.module.scss'
+
+import Image from 'next/image';
+import allCateIcon from '../../assets/icons/all-categories.svg'
 
 function NavBar() {
   return (
-    <div>NavBar</div>
-  )
+    <nav className={style['navbar']}>
+      <div>
+      <Link href="" className={style['all-categories']}>
+        <div>
+          <Image src={allCateIcon} alt='' />
+        </div>
+        All Categories</Link>
+      <Link href="">Furnitures</Link>
+      <Link href="">Phones</Link>
+      <Link href="">Laptops</Link>
+      <Link href="">Fashion</Link>
+      <Link href="">Baby & Toys</Link>
+      <Link href="">Gaming</Link>
+      <Link href="">Sports & Fitness</Link>
+      <Link href="">Home & Office</Link>
+      <Link href="">Health & Beauty</Link>
+      <Link href="">SuperMarkets</Link>
+      <Link href="">SuperMarkets</Link>
+        
+      </div>
+    </nav>
+  );
 }
 
 export default NavBar
